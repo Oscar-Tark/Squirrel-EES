934316c7468358f5ae7a408b188314998d94d7fc
Oscar-Tark <oscartark91@gmail.com>
2020-07-20 08:02:10 +0200

    SQLlite added

--- Scorpion/Database/Scorpion_DB.cs
+++ Scorpion/Database/Scorpion_DB.cs
@@ -16,11 +16,23 @@
 */
 
 using System.Collections;
-using System.IO;
+using System.Data.SQLite;
 
 //Static Library
 namespace Scorpion
 {
+    class SQLfunctions
+    { 
+        public string clean_sql_single_word(ref string sql)
+        {
+            sql = sql.Replace("'", "");
+            sql = sql.Replace("\"", "");
+            sql = sql.Replace(" ", "");
+            return sql;
+        }
+
+    }
+
     partial class Librarian
     {
         /*BASIC DB FUNCTIONS FOR INTERNAL DB
@@ -34,92 +46,131 @@ namespace Scorpion
          * ----------
          * {name, value}        
         */
-        public void dbcreate(ref string Scorp_line_Exec, ref ArrayList objects)
+
+        public void dbcreate(ref string Scorp_Line_Exec, ref ArrayList objects)
         {
-            //::*File_Name_w_path, *pwd
-            //MAX TABLE LEN IS 0x3a
-            //{val}
-            string name = (string)var_get(objects[0]);
-            //Could directly do a new Arraylist(), but want to dispose of it after the operation is done.
-            ArrayList al_db = new ArrayList();
-                Do_on.AL_TBLE.Add(new string[0x3a]);
-                Do_on.AL_TBLE_REF.Add(name);
-
-                File.WriteAllBytes(name, Do_on.crypto.encrypt(al_db, (string)var_get(objects[1])));
-                Do_on.write_to_cui("Created table file(to disk) : " + name);
-
-            name = null;
-            var_arraylist_dispose(ref al_db);
-            var_arraylist_dispose(ref objects);
-            Scorp_line_Exec = null;
-            return;
-        }
-
-        public void dbopen(string Scorp_line_Exec, ArrayList objects)
-        {
-            //::*path, *pwd
-            //MAX TABLE LEN IS 0x3a
-            string name = (string)var_get(objects[0]);
-
-            if (!Do_on.AL_TBLE_REF.Contains((string)var_get(objects[0])))
-            {
-                Do_on.AL_TBLE_REF.Add(var_get(objects[0]));
-
-                byte[] b = File.ReadAllBytes(name);
-                b = Do_on.crypto.decrypt(b, (string)var_get(objects[1]));
-
-                if (!verifydb(ref b))
-                    return;
-
-                Do_on.AL_TBLE.Add(Do_on.crypto.To_Object(new MemoryStream(b)));
-                Do_on.AL_TBLE_REF.Add(name);
-                //Do_on.AL_TBLE.Add(Do_on.vds.UnDump_DB(var_get(objects[0].ToString()).ToString(), Do_on.SHA));
-                //verifyload(var_get(objects[0].ToString()).ToString());
-                Do_on.write_to_cui("Added Data File: '" + var_get(objects[0]) + "'");
-            }
-            else { Do_on.write_to_cui("Table '" + var_get(objects[0]) + "' already in memory"); }
+            //::*path
+            SQLiteConnection.CreateFile((string)var_get(objects[0]));
 
             var_arraylist_dispose(ref objects);
-            Scorp_line_Exec = null;
-            return;
-        }
-
-        public void listdbs(ref string Scorp_Line_Exec, ref ArrayList objects)
-        {
-            foreach (string s_name in Do_on.AL_TBLE_REF)
-                Do_on.write_to_cui(s_name);
-
             Scorp_Line_Exec = null;
-            var_arraylist_dispose(ref objects);
             return;
         }
 
-        private bool verifydb(ref byte[] b)
+        public void dbtablecreate(ref string Scorp_Line_Exec, ref ArrayList objects)
         {
-            if(b.Length == 0x3a)
-                return true;
-            return false;
+            //::*path, *table name, *column, *column...
+
+            SQLfunctions s_fun = new SQLfunctions();
+            string Database = (string)var_get(objects[0]);
+            SQLiteConnection db_conn = new SQLiteConnection("Data Source=" + Database + "; Version=3");
+            db_conn.Open();
+
+            string Table = (string)var_get(objects[0]);
+            Table = s_fun.clean_sql_single_word(ref Table);
+
+            SQLiteCommand cmd = new SQLiteCommand("CREATE TABLE ", db_conn);
+            cmd.ExecuteNonQuery();
+
+            db_conn.Close();
+
+            s_fun = null;
+            db_conn = null;
+            cmd = null;
+            var_arraylist_dispose(ref objects);
+            Scorp_Line_Exec = null;
+            return;
         }
 
         /*
-        public void dbdelete(String Scorp_Line_Exec, ArrayList objects)
-        {
-            File.Delete(Do_on.AL_DIRECTORIES[0] + var_get(objects[0].ToString()).ToString() + Do_on.AL_EXTENSNS[1]);
-            Do_on.write_to_cui("Deleteing data file(from disk): " + var_get(objects[0].ToString()).ToString() + Do_on.AL_EXTENSNS[1]);
 
-            var_arraylist_dispose(ref objects);
-            Scorp_Line_Exec = null;
-            return;
+        //OLD
+    public void dbcreate(ref string Scorp_line_Exec, ref ArrayList objects)
+    {
+        //::*File_Name_w_path, *pwd
+        //MAX TABLE LEN IS 0x3a
+        //{val}
+        string name = (string)var_get(objects[0]);
+        //Could directly do a new Arraylist(), but want to dispose of it after the operation is done.
+        ArrayList al_db = new ArrayList();
+            Do_on.AL_TBLE.Add(new string[0x3a]);
+            Do_on.AL_TBLE_REF.Add(name);
+
+            File.WriteAllBytes(name, Do_on.crypto.encrypt(al_db, (string)var_get(objects[1])));
+            Do_on.write_to_cui("Created table file(to disk) : " + name);
+
+        name = null;
+        var_arraylist_dispose(ref al_db);
+        var_arraylist_dispose(ref objects);
+        Scorp_line_Exec = null;
+        return;
+    }
+
+    public void dbopen(string Scorp_line_Exec, ArrayList objects)
+    {
+        //::*path, *pwd
+        //MAX TABLE LEN IS 0x3a
+        string name = (string)var_get(objects[0]);
+
+        if (!Do_on.AL_TBLE_REF.Contains((string)var_get(objects[0])))
+        {
+            Do_on.AL_TBLE_REF.Add(var_get(objects[0]));
+
+            byte[] b = File.ReadAllBytes(name);
+            b = Do_on.crypto.decrypt(b, (string)var_get(objects[1]));
+
+            if (!verifydb(ref b))
+                return;
+
+            Do_on.AL_TBLE.Add(Do_on.crypto.To_Object(new MemoryStream(b)));
+            Do_on.AL_TBLE_REF.Add(name);
+            //Do_on.AL_TBLE.Add(Do_on.vds.UnDump_DB(var_get(objects[0].ToString()).ToString(), Do_on.SHA));
+            //verifyload(var_get(objects[0].ToString()).ToString());
+            Do_on.write_to_cui("Added Data File: '" + var_get(objects[0]) + "'");
         }
+        else { Do_on.write_to_cui("Table '" + var_get(objects[0]) + "' already in memory"); }
 
-        public void dbsave(string Scorp_Line_Exec, ArrayList objects)
-        {
-            Do_on.vds.Dump_DB(var_get(objects[0].ToString()).ToString());
+        var_arraylist_dispose(ref objects);
+        Scorp_line_Exec = null;
+        return;
+    }
 
-            var_arraylist_dispose(ref objects);
-            Scorp_Line_Exec = null;
-            return;
-        }*/
+    public void listdbs(ref string Scorp_Line_Exec, ref ArrayList objects)
+    {
+        foreach (string s_name in Do_on.AL_TBLE_REF)
+            Do_on.write_to_cui(s_name);
+
+        Scorp_Line_Exec = null;
+        var_arraylist_dispose(ref objects);
+        return;
+    }
+
+    private bool verifydb(ref byte[] b)
+    {
+        if(b.Length == 0x3a)
+            return true;
+        return false;
+    }
+
+    /*
+    public void dbdelete(String Scorp_Line_Exec, ArrayList objects)
+    {
+        File.Delete(Do_on.AL_DIRECTORIES[0] + var_get(objects[0].ToString()).ToString() + Do_on.AL_EXTENSNS[1]);
+        Do_on.write_to_cui("Deleteing data file(from disk): " + var_get(objects[0].ToString()).ToString() + Do_on.AL_EXTENSNS[1]);
+
+        var_arraylist_dispose(ref objects);
+        Scorp_Line_Exec = null;
+        return;
+    }
+
+    public void dbsave(string Scorp_Line_Exec, ArrayList objects)
+    {
+        Do_on.vds.Dump_DB(var_get(objects[0].ToString()).ToString());
+
+        var_arraylist_dispose(ref objects);
+        Scorp_Line_Exec = null;
+        return;
+    }*/
 
         //OLD REMOVE
         /*private void verifyload(string Name)
