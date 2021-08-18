762d32eabdc23aad75a368a07e0df5471f414c06
Oscar-Tark <oscartark91@gmail.com>
2021-08-18 20:55:57 +0200

    Added ASP server project

---
+++ Scorpion_ASPNET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="Scorpion_ASPNET.Global" %>
---
+++ Scorpion_ASPNET/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace Scorpion_ASPNET
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
