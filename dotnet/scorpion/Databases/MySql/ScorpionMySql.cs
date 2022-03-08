650ea767a7e5e8af63ef3d481ca4e26c9bed8705
Oscar-Tark <oscartark91@gmail.com>
2022-03-08 17:31:15 +0100

    Added Mysql: new, get functionality

---
+++ dotnet/scorpion/Databases/MySql/ScorpionMySql.cs
@@ -0,0 +1,84 @@
+/*  <Scorpion IEE(Intelligent Execution Environment). Server To Run Scorpion Built Applications Using the Scorpion Language>
+    Copyright (C) <2020+>  <Oscar Arjun Singh Tark>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.IO;
+using System.Collections;
+
+namespace Scorpion
+{
+    public partial class Librarian
+    {
+        public void mysqlnew(ref string Scorp_Line_Exec, ref ArrayList objects)
+        {
+            //Creates a new generic data table
+            //Table format: [id:int] [path:string] [identifier(name, age...):string] [data:string]
+            //::*connectionstringvar, *tablename
+            //var::*con >> *con<<mysqlcreatestring::*'localhost', *'3306', *'scorpion_iee', *'root', *'' >> mysqlnew::*con, *'test'
+            using(var mysql = new ScorpionMySql.ScorpionSql())
+            {
+                mysql.sqlnew((string)var_get(objects[0]), (string)var_get(objects[1]));
+            }
+
+            var_dispose_internal(ref Scorp_Line_Exec);
+            var_arraylist_dispose(ref objects);
+            return;
+        }
+
+        public string mysqlcreatestring(ref string Scorp_Line_Exec, ref ArrayList objects)
+        {
+            //var::*con >> *con<<mysqlcreatestring::*'localhost', *'3306', *'scorpion_iee', *'root', *'' >> mysqltest::*con
+            //*connectionstringvar<<::*'host', *'port', *'db', *'uname', *'password'
+            //Creates a connection string to the specified variable
+
+            string returnable = string.Format("server={0};port={1};userid={2};password={3};database={4}", (string)var_get(objects[0]), (string)var_get(objects[1]), (string)var_get(objects[3]), (string)var_get(objects[4]), (string)var_get(objects[2]));
+
+            var_dispose_internal(ref Scorp_Line_Exec);
+            var_arraylist_dispose(ref objects);
+            return var_create_return(returnable, true);
+        }
+
+        public object mysqlget(ref string Scorp_Line_Exec, ref ArrayList objects)
+        {
+            //Inserts data into MySql table
+            //Table format: [id:int] [path:string] [identifier(name, age...):string] [data:string]
+            //*returnable<<*connectionstringvar, *table, *[path], *[identifier], *conditional_[data]_parameter
+            //var::*con >> *con<<mysqlcreatestring::*'localhost', *'3306', *'scorpion_iee', *'root', *'' >> mysqlget::*con, *'test', *'/testapp', *'testdata'
+            object returnable = Do_on.types.S_NULL;
+            using(var mysql = new ScorpionMySql.ScorpionSql())
+            {
+                returnable = mysql.sqlGet((string)var_get(objects[0]), (string)var_get(objects[1]), (string)var_get(objects[2]), (string)var_get(objects[3]), (string)var_get(objects[4]));
+            }
+
+            var_dispose_internal(ref Scorp_Line_Exec);
+            var_arraylist_dispose(ref objects);
+            return var_create_return(ref returnable);
+        }
+
+        public void mysqltest(ref string Scorp_Line_Exec, ref ArrayList objects)
+        {
+            using(var mysql = new ScorpionMySql.ScorpionSql())
+            {
+                mysql.test((string)var_get(objects[0]));
+            }
+
+            var_dispose_internal(ref Scorp_Line_Exec);
+            var_arraylist_dispose(ref objects);
+            return;
+        }
+    }
+}
--- dotnet/scorpion_sql/ScorpionSQL.cs
+++ dotnet/scorpion_sql/ScorpionSQL.cs
@@ -2,9 +2,82 @@
 * SQL sructure
 * [id:int] [path:string] [identifier(name, age...):string] [data:string]
 */
+using MySqlConnector;
+using System.ComponentModel;
 
-namespace scorpionSql;
-public class ScorpionSql
+namespace ScorpionMySql
 {
+public class ScorpionSql:IDisposable
+{
+    public object sqlGet(string connection_string, string table, string path, string data)
+    {
+        using (var connection = new MySqlConnection(connection_string))
+        {
+            connection.Open();
+            using (var command = new MySqlCommand(string.Format("SELECT * FROM {0} WHERE path=@path AND data=@data;", table), connection))
+            {
+                command.Parameters.AddWithValue("path", path);
+                command.Parameters.AddWithValue("data", data);
+                using (var reader = command.ExecuteReader())
+                    while (reader.Read())
+                        Console.WriteLine(reader.GetString(0));
+            }
+        }
+        return null;
+    }
 
+    public void sqlSet()
+    {
+
+    }
+
+    public void sqlnew(string connection_string, string table_name)
+    {
+        //Creates a new generic data table
+        //[id:int] [path:string] [identifier(name, age...):string] [data:string]
+        
+        using (var connection = new MySqlConnection(connection_string))
+        {
+            connection.Open();
+            using (var command = new MySqlCommand(string.Format("CREATE TABLE {0} (id INT NOT NULL AUTO_INCREMENT, path VARCHAR(128) NOT NULL, identifier VARCHAR(32) NOT NULL, data VARCHAR(2048) NULL, PRIMARY KEY (id))", table_name), connection))
+            {
+
+                try
+                   {
+                    command.ExecuteNonQuery();
+                }
+                catch(System.Exception e){ System.Console.WriteLine(e.Message); }
+            }
+        }
+    }
+
+    //System functions
+    private static string sqlSanitize()
+    {
+        return null;
+    }
+
+    public void test(string connection_string)
+    {
+            using (var connection = new MySqlConnection(connection_string))
+            {
+                try
+                {
+                connection.Open();
+                using (var command = new MySqlCommand("SELECT json FROM module_users;", connection))
+                using (var reader = command.ExecuteReader())
+                    while (reader.Read())
+                        Console.WriteLine(reader.GetString(0));
+                connection.Close();        
+                }
+                catch(System.Exception e)
+                { System.Console.WriteLine(e.Message); }
+            }
+    }
+
+    public void Dispose()
+    {
+    
+    }
+}
 }
